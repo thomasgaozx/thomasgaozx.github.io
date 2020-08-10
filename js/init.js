9f9791047b981e83e0020104be997891af709e66
Thomas Gao <thomasgaozx@gmail.com>
2020-08-10 10:58:03 -0400

    Test new button.

--- js/init.js
+++ js/init.js
@@ -20,6 +20,27 @@ var libs = String.raw`
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.css" integrity="sha384-yFRtMMDnQtDRO8rLpMIKrtPCD5jdktao2TV19YiZYWMDkUR5GQZR/NOVTdquEx1j" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/katex-copytex@latest/dist/katex-copytex.min.css" rel="stylesheet" type="text/css">
 <script src="https://cdn.jsdelivr.net/npm/katex-copytex@latest/dist/katex-copytex.min.js"></script>
+<script>
+// From https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 `;
 
 // load libraries
@@ -40,5 +61,32 @@ blockquote{
     position: relative;
     background:#EDEDED;
 }
+#myBtn {
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    font-size: 18px;
+    border: none;
+    outline: none;
+    background-color: brown;
+    color: white;
+    cursor: pointer;
+    padding: 15px;
+    border-radius: 4px;
+}
+
+#myBtn:hover {
+    background-color: #555;
+}
 `;
+
+var scroll_to_top = document.createElement('button')
+scroll_to_top.setAttribute("id", "myBtn")
+scroll_to_top.setAttribute("onclick", "topFunction()")
+scroll_to_top.setAttribute("title", "Go to top")
+scroll_to_top.innerText = "Back To Top"
+
 document.getElementsByTagName('head')[0].appendChild(custom_style);
+document.getElementsByTagName('body')[0].appendChild(scroll_to_top);
